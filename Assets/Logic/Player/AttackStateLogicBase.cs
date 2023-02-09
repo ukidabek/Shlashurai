818f0ff7e439fb150982dc8151d521336da84ca9
Łukasz <ukidabek@gmail.com>
2023-02-09 12:52:01 +0100

    [Code] States logic refactor to better handle animation logic.

---
+++ Assets/Logic/Player/AttackStateLogicBase.cs
@@ -0,0 +1,9 @@
+﻿using Utilities.States;
+
+namespace Shlashurai.Player.Logic
+{
+	public abstract class AttackStateLogicBase : StateLogicMonoBehaviour
+	{
+		public abstract bool PerformingAttack { get; protected set; }
+	}
+}
---
+++ Assets/Logic/Player/MovementAnimationStateLogic.cs
@@ -0,0 +1,22 @@
+﻿using Shlashurai.Player.Input;
+using UnityEngine;
+using Utilities.General;
+using Utilities.States;
+
+namespace Shlashurai.Player.Logic
+{
+	public class MovementAnimationStateLogic : StateLogicMonoBehaviour, IOnUpdateLogic
+	{
+		[SerializeField] private AnimatorParameterDefinition m_movementParameterDefinition = null;
+        [SerializeField] private Animator m_animator = null;
+		[SerializeField] private InputValues m_inputValues = null;
+
+        private float m_input;
+
+		public void OnUpdate(float deltaTime)
+        {
+			m_input = m_inputValues.Move.magnitude;
+            m_movementParameterDefinition.SetFloat(m_animator, m_input);
+		}
+	}
+}
