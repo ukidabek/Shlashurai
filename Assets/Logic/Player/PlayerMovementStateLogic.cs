fd48c76e0ca380b3ee0ffd72fc5ad1ff961e2d40
Łukasz <ukidabek@gmail.com>
2023-02-10 12:12:15 +0100

    [Code] Player movement direction fixed.
    Player preserve movement direction.

---
+++ Assets/Logic/Player/PlayerMovementStateLogic.cs
@@ -0,0 +1,73 @@
+using Shlashurai.Player.Input;
+using UnityEngine;
+using UnityEngine.Windows;
+using Utilities.States;
+
+namespace Shlashurai.Player.Logic
+{
+	public class PlayerMovementStateLogic : StateLogicMonoBehaviour, IOnUpdateLogic, IOnFixUpdateLogic
+	{
+		[SerializeField] private Transform m_root = null;
+		[SerializeField] private Transform m_model = null;
+		[SerializeField] private InputValues m_inputValues = null;
+		[SerializeField] private Rigidbody m_controlelr = null;
+		[SerializeField] private float m_speed = 3;
+		[SerializeField] private bool m_freazDirections = false;
+		[SerializeField, Range(0, 1f)] float m_slerpOverRange = 1f;
+
+		private Vector3 m_frezedForwardDirection, m_frezedRightDirection, m_input, m_normalInput, m_reflectedInput;
+
+		public override void Activate()
+		{
+			base.Activate();
+
+			m_frezedForwardDirection = m_root.forward;
+			m_frezedRightDirection = m_root.right;
+		}
+
+		public void OnUpdate(float deltaTime)
+		{
+			var move = m_inputValues.Move;
+
+			m_normalInput.Set(move.x, 0, move.y);
+			m_normalInput.Normalize();
+			m_reflectedInput = Vector3.Reflect(m_normalInput, m_root.right);
+
+			var rootForward = m_root.forward;
+			var dot = Vector3.Dot(m_root.forward, m_model.forward);
+
+			var slerpValue = Mathf.InverseLerp(m_slerpOverRange, -m_slerpOverRange, dot);
+			m_input = Vector3.Slerp(m_normalInput, m_reflectedInput, slerpValue);
+
+#if UNITY_EDITOR
+			var position = m_root.position;
+			Debug.DrawRay(position, m_normalInput, Color.gray);
+			Debug.DrawRay(position, m_input, Color.red);
+			Debug.DrawRay(position, m_reflectedInput, Color.black);
+#endif
+		}
+
+		public void OnFixUpdate(float deltaTime)
+		{
+			var forward = m_freazDirections ? m_frezedForwardDirection : m_model.forward;
+			var right = m_freazDirections ? m_frezedRightDirection : m_model.right;
+
+#if UNITY_EDITOR
+			var position = m_root.position;
+			Debug.DrawRay(position, forward, Color.blue);
+			Debug.DrawRay(position, right, Color.red);
+#endif
+
+			forward *= m_speed * m_input.z;
+			right *= m_speed * m_input.x;
+
+			var velocity = forward + right;
+			m_controlelr.velocity = velocity;
+
+#if UNITY_EDITOR
+			Debug.DrawRay(position, velocity, Color.cyan);
+			Debug.DrawRay(position, m_controlelr.velocity, Color.yellow);
+#endif
+		}
+	}
+}
