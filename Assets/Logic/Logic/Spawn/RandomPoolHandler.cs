a1b4712ae0517a6ff986a33fae67bcb2d40a67b8
Łukasz <ukidabek@gmail.com>
2023-02-14 10:51:33 +0100

    [Code] Spawn logic refactor for enabling more elastic usage.

---
+++ Assets/Logic/Logic/Spawn/RandomPoolHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Utilities.Pool;
+
+namespace Shlashurai.Spawn
+{
+	public class RandomPoolHandler : PoolHandler<GameObjectPool, GameObject> 
+	{
+		[SerializeField] private float m_chance = 1.0f;
+		public float Chance => m_chance;
+		private Vector2 m_range = Vector2.zero;
+
+		public override void Initialize(Transform parent, float previousChande)
+		{
+			base.Initialize(parent, previousChande);
+			m_range.Set(previousChande, previousChande + m_chance);
+		}
+
+		public bool IsInRange(float roll) => m_range.x <= roll && m_range.y >= roll;
+	}
+}
---
+++ Assets/Logic/Logic/Spawn/SpawnBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using Utilities.Pool;
+
+namespace Shlashurai.Spawn
+{
+	public abstract class SpawnBase<PoolT, ObjectT, PoolHandlerT> : MonoBehaviour
+		where PoolT : Pool<ObjectT>, new() 
+		where ObjectT : UnityEngine.Object
+		where PoolHandlerT : PoolHandler<PoolT, ObjectT>, new()
+	{
+		[SerializeField] protected PoolHandlerT[] m_objectToSpawn = Array.Empty<PoolHandlerT>();
+	}
+}
