385008384f3b24eb3c6fa8699c7018946cd458d0
Łukasz <ukidabek@gmail.com>
2023-02-14 10:56:02 +0100

    [Code] ContainerDamagable component added.

---
+++ Assets/Logic/ContainerDamagable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Utilities.Containers;
+using Weapons;
+
+namespace Shlashurai.Containers
+{
+	[RequireComponent(typeof(Container))]
+	public class ContainerDamagable : MonoBehaviour, IDamageable
+	{
+		[SerializeField] private Container m_container = null;
+
+		public void ReceiveDamage(IDamage damage)
+		{
+			m_container.Open();
+		}
+
+		private void Reset()
+		{
+			m_container = GetComponent<Container>();
+		}
+	}
+}
