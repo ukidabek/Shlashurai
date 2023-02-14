1c6848f5ec356e5ab01ee084444daeadcab22123
Łukasz <ukidabek@gmail.com>
2023-02-14 10:48:18 +0100

    [Code] PickUpable interface PickUp method returns object now. To enable more elastic use cases.

--- Assets/Utilities/Interactions/PickUpHandlerBase.cs
+++ Assets/Utilities/Interactions/PickUpHandlerBase.cs
@@ -4,13 +4,13 @@ namespace Utilities.Interactions
 {
 	public abstract class PickUpHandlerBase : MonoBehaviour
 	{
-		public abstract bool CanHandle(IPickUpable pickUp);
-		public abstract void Handle(IPickUpable pickUp);
+		public abstract bool CanHandle(object pickUp);
+		public abstract void Handle(object pickUp);
 	}
 
 	public abstract class PickUpHandlerBase<T> : PickUpHandlerBase where T : IPickUpable
 	{
-		public override bool CanHandle(IPickUpable pickUp) => pickUp is T;
-		protected T GetReference(IPickUpable pickUp) => (T)pickUp;
+		public override bool CanHandle(object pickUp) => pickUp is T;
+		protected T GetReference(object pickUp) => (T)pickUp;
 	}
 }
