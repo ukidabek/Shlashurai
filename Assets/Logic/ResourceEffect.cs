cc0f54eccd45bac8cf8d7ca97ad7b9ba647c3b3a
Łukasz <ukidabek@gmail.com>
2023-02-17 10:53:56 +0100

    [Code] Classes renamed to more generic names.

--- Assets/Logic/ResourceEffect.cs
+++ Assets/Logic/ResourceEffect.cs
@@ -1,11 +1,14 @@
-﻿using UnityEngine;
+﻿using Shlashurai.Characters;
+using UnityEngine;
 
 namespace Shlashurai.Consumable
 {
-
 	[CreateAssetMenu(menuName = "ConsumableEffect/HealEffect", fileName = "HealEffect")]
-	public class HealEffect : ConsumableEffect
+	public class ResourceEffect : ConsumableEffect
 	{
+		[SerializeField] private ResourceID m_resourceID = null;
+		public ResourceID ResourceID => m_resourceID;
+
 		[SerializeField] private float m_healAmount = 10f;
 		public float HealAmount => m_healAmount;
 	}
---
+++ Assets/Logic/ResourceEffectHandler.cs
@@ -0,0 +1,20 @@
+﻿using Shlashurai.Characters;
+using UnityEngine;
+using Utilities.Consumable;
+
+namespace Shlashurai.Consumable
+{
+	public class ResourceEffectHandler : MonoBehaviour, IConsumableEffectHandlr
+	{
+		[SerializeField] private ResourceManager m_characterHealth = null;
+
+		public bool CanHandle(IConsumableEffect effect) => effect is ResourceEffect;
+
+		public void Handle(IConsumableEffect effect)
+		{
+			var resourceEffect = effect as ResourceEffect;
+			var helth = m_characterHealth.GetResource(resourceEffect.ResourceID);
+			helth.Value += resourceEffect.HealAmount;
+		}
+	}
+}
