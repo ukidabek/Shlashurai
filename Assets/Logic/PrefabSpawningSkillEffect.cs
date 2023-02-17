6fd1041646df50db312691093014067055cfd631
Łukasz <ukidabek@gmail.com>
2023-02-17 13:50:04 +0100

    [Code] Spell visual effect handling logic added.

--- Assets/Logic/PrefabSpawningSkillEffect.cs
+++ Assets/Logic/PrefabSpawningSkillEffect.cs
@@ -1,17 +1,23 @@
 ﻿using Shlashurai.Skill;
+using Shlashurai.Spawn;
 using System.Collections;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Skills/Effects/PrefabSpawningSkillEffect", fileName = "PrefabSpawningSkillEffect")]
 public class PrefabSpawningSkillEffect : SkillEffect
 {
-	[SerializeField] private GameObject m_prefab = null;
+	[SerializeField] private bool m_spawnOnSelf = false;
+	[SerializeField] private SpellEffectSpawn m_skillEffectSpawn = null;
+
 	public override IEnumerator Affect(SkillCastManager skillCastManager, GameObject target)
 	{
-		var spawnPoint = skillCastManager.SkillSpawnPoint;
-		var instance = GameObject.Instantiate(m_prefab, spawnPoint.position, spawnPoint.rotation);
+		var effect = m_skillEffectSpawn.GetItemInstance(this);
+		var effectTransform = effect.transform;
+		var skillSpawnTransform = m_spawnOnSelf ? skillCastManager.transform : skillCastManager.SkillSpawnPoint;
+		effectTransform.position = skillSpawnTransform.position;
+		effectTransform.rotation = skillSpawnTransform.rotation;
 
-		instance.SetActive(true);
+		effect.gameObject.SetActive(true);
 
 		return null;
 	}
