eb0151365f800c2127705496ec604cf83120b897
Łukasz <ukidabek@gmail.com>
2023-02-17 13:43:53 +0100

    [Code] Skill effect utility components added.

---
+++ Assets/Logic/SkillEffectsUtilities/AfterTimeObjectDisabler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Logic.SkillEffectsUtilities
+{
+	public class AfterTimeObjectDisabler : MonoBehaviour
+	{
+		[SerializeField] private float m_timeToWait = 1f;
+
+		private WaitForSeconds m_wait = null;
+
+		private void Awake() => m_wait = new WaitForSeconds(m_timeToWait);
+
+		private void OnEnable() => StartCoroutine(WaitAndDisable());
+
+		private IEnumerator WaitAndDisable()
+		{
+			yield return m_wait;
+			gameObject.SetActive(false);
+		}
+	}
+}
