32ab09f5c17a791b254e72887a7c4dc168903db1
Łukasz <ukidabek@gmail.com>
2023-02-17 10:59:05 +0100

    [Code] State logic for resource handling added.

---
+++ Assets/Logic/Logic/States/ConsumeResourceStateLogic.cs
@@ -0,0 +1,18 @@
+﻿using Shlashurai.Characters;
+using UnityEngine;
+using Utilities.States;
+
+namespace Shlashurai.Character
+{
+	public class ConsumeResourceStateLogic : StateLogicMonoBehaviour
+	{
+		[SerializeField] private ResourceChandler m_resourceChandler = null;
+		[SerializeField] private float m_consume = 10f;
+
+		public override void Activate()
+		{
+			base.Activate();
+			m_resourceChandler.Value -= m_consume;
+		}
+	}
+}
---
+++ Assets/Logic/Logic/States/ResourceSwitchStateCondition.cs
@@ -0,0 +1,15 @@
+﻿using Shlashurai.Characters;
+using UnityEngine;
+using Utilities.States;
+
+namespace Shlashurai.Character
+{
+	public class ResourceSwitchStateCondition : SwitchStateConditionBase
+	{
+		[SerializeField] private ResourceChandler m_resourceChandler = null;
+
+		[SerializeField] private float m_minimumValue = 10f;
+
+		public override bool Condition => m_resourceChandler.Value >= m_minimumValue;
+	}
+}
