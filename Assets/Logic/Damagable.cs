01899d8152bc52b988318cad977f1970bf5d554f
Łukasz <ukidabek@gmail.com>
2023-02-17 10:53:56 +0100

    [Code] Classes renamed to more generic names.

---
+++ Assets/Logic/Damagable.cs
@@ -0,0 +1,13 @@
+﻿using Shlashurai.Characters;
+using UnityEngine;
+using Weapons;
+
+[RequireComponent(typeof(ResourceManager))]
+public class Damagable : MonoBehaviour, IDamageable
+{
+	[SerializeField] private ResourceChandler m_resourceChandler = null;
+
+	public void ReceiveDamage(IDamage damage) => m_resourceChandler.Value -= damage.Amount;
+
+	private void Reset() => m_resourceChandler.ResourceManager = GetComponent<ResourceManager>();
+}
