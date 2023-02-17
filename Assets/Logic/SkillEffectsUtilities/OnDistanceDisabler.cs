7af2daf153cf7a8c1fe7d0bae0e31b73456f49dc
Łukasz <ukidabek@gmail.com>
2023-02-17 16:18:12 +0100

    [Code] RigidBodySpeedSetter spited to two classes. OnDistanceDisabler handle by distance object disabling.

---
+++ Assets/Logic/SkillEffectsUtilities/OnDistanceDisabler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Logic.SkillEffectsUtilities
+{
+	public class OnDistanceDisabler : MonoBehaviour
+	{
+		[SerializeField] private float m_maxDistance = 10;
+		private Vector3 m_startPosition = Vector3.zero;
+		private void OnEnable()
+		{
+			m_startPosition = transform.position;
+		}
+
+		private void Update()
+		{
+			var position = transform.position;
+
+			if (Vector3.Distance(m_startPosition, position) >= m_maxDistance)
+				gameObject.SetActive(false);
+		}
+	}
+}
