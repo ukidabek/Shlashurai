417291f7abd8f0022c78acc5b2722bf3122a6e03
Łukasz <ukidabek@gmail.com>
2023-02-17 11:29:52 +0100

    [Code] Prefab spawning skill effect logic added.

---
+++ Assets/Logic/PrefabSpawningSkillEffect.cs
@@ -0,0 +1,18 @@
+﻿using Shlashurai.Skill;
+using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Skills/Effects/PrefabSpawningSkillEffect", fileName = "PrefabSpawningSkillEffect")]
+public class PrefabSpawningSkillEffect : SkillEffect
+{
+	[SerializeField] private GameObject m_prefab = null;
+	public override IEnumerator Affect(SkillCastManager skillCastManager, GameObject target)
+	{
+		var spawnPoint = skillCastManager.SkillSpawnPoint;
+		var instance = GameObject.Instantiate(m_prefab, spawnPoint.position, spawnPoint.rotation);
+
+		instance.SetActive(true);
+
+		return null;
+	}
+}
