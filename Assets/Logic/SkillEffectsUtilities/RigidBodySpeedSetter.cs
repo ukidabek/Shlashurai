6fd7b7143d6cfd5d3e751d7d0a45d7dc329aeeb3
Łukasz <ukidabek@gmail.com>
2023-02-17 16:18:12 +0100

    [Code] RigidBodySpeedSetter spited to two classes. OnDistanceDisabler handle by distance object disabling.

--- Assets/Logic/SkillEffectsUtilities/RigidBodySpeedSetter.cs
+++ Assets/Logic/SkillEffectsUtilities/RigidBodySpeedSetter.cs
@@ -6,22 +6,11 @@ namespace Logic.SkillEffectsUtilities
 	{
 		[SerializeField] private Rigidbody m_rigidbody = null;
 		[SerializeField] private float m_speed = 5;
-		[SerializeField] private float m_maxDistance = 10;
 
-		private Vector3 m_startPosition = Vector3.zero;
 
 		private void OnEnable()
 		{
-			m_startPosition = transform.position;
 			m_rigidbody.velocity = transform.forward * m_speed;
-		}
-
-		private void Update()
-		{
-			var position = transform.position;
-
-			if (Vector3.Distance(m_startPosition, position) >= m_maxDistance)
-				gameObject.SetActive(false);
 		}
 	}
 }
