277ce27edbd042f463dde026333ae7b8685f9fca
Łukasz <ukidabek@gmail.com>
2023-02-17 10:57:00 +0100

    [Code] Item helper class extracted to dedicated file.

---
+++ Assets/Logic/Logic/Items/ItemHelperClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Shlashurai.Items
+{
+	public static class ItemHelperClass
+	{
+		public static T GetComponet<T>(this IItem item)
+		{
+			var type = typeof(T);
+			var components = item.Components;
+			var component = components.FirstOrDefault(component => component.GetType() == typeof(T));
+			return component != null ? (T)component : default;
+		}
+	}
+}
