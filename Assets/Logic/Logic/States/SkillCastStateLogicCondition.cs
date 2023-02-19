1ccbaa3dd1d82fb15891653134ee9b4707314b0e
Łukasz <ukidabek@gmail.com>
2023-02-19 13:37:29 +0100

    [Code] Skill handling state logic added.

---
+++ Assets/Logic/Logic/States/SkillCastStateLogicCondition.cs
@@ -0,0 +1,13 @@
+﻿using Shlashurai.Skill;
+using UnityEngine;
+using Utilities.States;
+
+namespace Shlashurai.Player.Logic
+{
+	public class SkillCastStateLogicCondition : StateLogic, ISwitchStateCondition
+	{
+		[SerializeField] private SkillCastManager m_skillCastManager = null;
+
+		public bool Condition => !m_skillCastManager.Casting;
+	}
+}
