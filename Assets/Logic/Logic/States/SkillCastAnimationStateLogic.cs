91bb5a3c122bee8bbf6afd086c04028d90b32ed6
Łukasz <ukidabek@gmail.com>
2023-02-19 13:37:29 +0100

    [Code] Skill handling state logic added.

---
+++ Assets/Logic/Logic/States/SkillCastAnimationStateLogic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Utilities.General;
+using Utilities.States;
+
+namespace Shlashurai.Player.Logic
+{
+	public class SkillCastAnimationStateLogic : StateLogic
+	{
+		[SerializeField] private Animator m_animator = null;
+		[SerializeField] private AnimatorParameterDefinition m_beginCastAnimatorParameter = null;
+		[SerializeField] private AnimatorParameterDefinition m_endCastAnimatorParameter = null;
+
+		public override void Activate()
+		{
+			base.Activate();
+			m_beginCastAnimatorParameter.SetTrigger(m_animator);
+		}
+
+		public override void Deactivate()
+		{
+			base.Deactivate();
+			m_endCastAnimatorParameter.SetTrigger(m_animator);
+		}
+	}
+}
---
+++ Assets/Logic/Logic/States/SkillCastStateLogic.cs
@@ -0,0 +1,40 @@
+﻿using Utilities.States;
+using UnityEngine;
+using Shlashurai.Player.Input;
+using Shlashurai.Skill;
+
+namespace Shlashurai.Player.Logic
+{
+	public class SkillCastStateLogic : StateLogic, IOnUpdateLogic, ISwitchStateCondition
+	{
+		[SerializeField] private InputValues m_inputValues = null;
+		[SerializeField] private SkillCastManager m_skillCastManager = null;
+		[SerializeField] private SkillHolder m_skillHolder = null;
+
+		private bool m_condition = false;
+		public bool Condition => m_condition;
+
+		public override void Activate()
+		{
+			base.Activate();
+			m_condition = false;
+		}
+
+		public void OnUpdate(float deltaTime, float timeScale)
+		{
+			m_inputValues.Cast1 = Cast(m_inputValues.Cast1, m_skillHolder.GetSkillSlot(0).Skill);
+			m_inputValues.Cast2 = Cast(m_inputValues.Cast2, m_skillHolder.GetSkillSlot(1).Skill);
+		}
+
+		private bool Cast(bool cast, ISkill skill)
+		{
+			if (cast)
+			{
+				m_skillCastManager.Cast(skill);
+				m_condition = true;
+				return false;
+			}
+			return cast;
+		}
+	}
+}
