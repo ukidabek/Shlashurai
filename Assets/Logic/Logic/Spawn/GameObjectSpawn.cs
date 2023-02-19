861cd0b67ea271ca5955d67469df6507d9724423
Łukasz <ukidabek@gmail.com>
2023-02-19 17:52:29 +0100

    [Code] Enemy spawn logic restored.

---
+++ Assets/Logic/Logic/Spawn/GameObjectSpawn.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Utilities.Pool;
+
+namespace Shlashurai.Spawn
+{
+	[CreateAssetMenu(menuName = "Spawn/Spawn/GameObjectSpawn", fileName = "GameObjectSpawn")]
+	public class GameObjectSpawn : SpawnBase<GameObjectPool, GameObject, GameObject, GameObjectPoolHander>
+	{
+	}
+}
