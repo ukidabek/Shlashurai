e86fb4863c6a6b6f41b470047e32e3fab0036a01
Łukasz <ukidabek@gmail.com>
2023-02-19 13:30:45 +0100

    [Code] Event on reference change in reference host added.

--- Assets/Utilities/ReferenceHost/ReferenceHost.cs
+++ Assets/Utilities/ReferenceHost/ReferenceHost.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -5,9 +6,20 @@ namespace Utilities.ReferenceHost
 {
     public abstract class ReferenceHost<T> : ScriptableObject where T : Object
     {
-        [SerializeField] private T m_instance = null;
-        public T Instance => m_instance;
+        public T Instance {get; private set;}
 
-        internal void SetReference(T reference) => m_instance = reference;
-    }
+        public event Action OnReferenceChanged = null;
+
+		internal void SetReference(T reference)
+		{
+			if (Instance == reference) return;
+			Instance = reference;
+			OnReferenceChanged?.Invoke();
+		}
+
+		private void OnDisable()
+		{
+			Instance = null;
+		}
+	}
 }
