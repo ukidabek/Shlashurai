c916963533e7e7d81e0a8a26f376a2f1d3c5aead
Łukasz <ukidabek@gmail.com>
2023-02-19 13:45:23 +0100

    [Code] CoolDown skill logic added.

---
+++ Assets/Logic/Logic/CoolDownStatus.cs
@@ -0,0 +1,22 @@
+﻿using Shlashurai.Skill;
+using System;
+
+public class CoolDownStatus : ISkillStatus
+{
+	private float m_initialCoolDownTime = 0f;
+	private float m_coolDownTime = 0f;
+
+	public event Action<float> OnCoolDownChanged = null;
+
+	public CoolDownStatus(float coolDownTime)
+	{
+		m_initialCoolDownTime = m_coolDownTime = coolDownTime;
+	}
+
+	public bool Tick(float deltaTime)
+	{
+		m_coolDownTime -= deltaTime;
+		OnCoolDownChanged?.Invoke(1 - (m_coolDownTime / m_initialCoolDownTime));
+		return m_coolDownTime <= 0f;
+	}
+}
