d10d567cad6fd757c570121b24a9aee7d20d6bea
Łukasz <ukidabek@gmail.com>
2023-02-20 14:36:17 +0100

    [Code] Enemy slider initialization logic fixed.

--- Assets/Dedicated Logic/UI/SliderDisplayInitializer.cs
+++ Assets/Dedicated Logic/UI/SliderDisplayInitializer.cs
@@ -4,7 +4,7 @@ public class SliderDisplayInitializer : MonoBehaviour
 {
     [SerializeField] private ResourceSliderDisplayModel m_sliderDisplayModel = null;
 
-	private void Awake()
+	private void Start()
 	{
 		m_sliderDisplayModel.Initialize();
 	}
