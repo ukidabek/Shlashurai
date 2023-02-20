9279f77da53fbd216c37dff420a7f0a9f2e24b5c
Łukasz <ukidabek@gmail.com>
2023-02-20 13:31:40 +0100

    [Code] PickUp and item logic updated to handle currency.

---
+++ Assets/Dedicated Logic/Items/CurrencyItemComponent.cs
@@ -0,0 +1,16 @@
+﻿using Money;
+using Shlashurai.Items;
+
+public class CurrencyItemComponent : IItemComponent
+{
+	private float m_amount = 10f;
+	private Currency m_currency = null;
+
+	public CurrencyItemComponent(float amount, Currency currency)
+	{
+		m_amount = amount;
+		m_currency = currency;
+	}
+
+	public void Apply() => m_currency.Amount += m_amount;
+}
---
+++ Assets/Dedicated Logic/PickUp/ConsumableItemPickUpHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Utilities.Consumable;
+
+namespace Shlashurai.PickUp
+{
+	public class ConsumableItemPickUpHandler : ItemPickUpHandler<IConsumable>
+	{
+		[SerializeField] private ConsumableHandler m_consumableHandler = null;
+
+		public override void Handle(object pickUp) => m_consumableHandler.Consume(m_itemComponent);
+	}
+}
