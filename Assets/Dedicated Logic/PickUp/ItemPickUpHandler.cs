518b12312557ad990411daac4daf79de6b65d8ea
Łukasz <ukidabek@gmail.com>
2023-02-20 11:39:04 +0100

    [Code] PickUp logic moved to dedicated code.

---
+++ Assets/Dedicated Logic/PickUp/ItemPickUpHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Utilities.Interactions;
+using Utilities.Consumable;
+using Shlashurai.Items;
+
+namespace Shlashurai.PickUp
+{
+	public class ItemPickUpHandler : PickUpHandlerBase
+	{
+		[SerializeField] private ConsumableHandler m_consumableHandler = null;
+
+		public override bool CanHandle(object pickUp) => pickUp is ItemBinder binder &&
+			binder.Item != null &&
+			binder.Item.GetComponent<IConsumable>() != null;
+
+		public override void Handle(object pickUp)
+		{
+			var item = pickUp as IItem;
+			var consumable = item.GetComponent<IConsumable>();
+			m_consumableHandler.Consume(consumable);
+		}
+	}
+}
