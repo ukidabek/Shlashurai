c7efaa9eb3bbe7ca7347eb66dabecded7b7a34b5
Łukasz <ukidabek@gmail.com>
2023-02-20 14:14:35 +0100

    [Code] Simple currency display logic added.

---
+++ Assets/Dedicated Logic/UI/CurrencyDisplay.cs
@@ -0,0 +1,27 @@
+using Money;
+using TMPro;
+using UnityEngine;
+
+public class CurrencyDisplay : MonoBehaviour
+{
+	[SerializeField] private Currency m_currency = null;
+	[SerializeField] private TMP_Text m_name = null;
+	[SerializeField] private TMP_Text m_amount = null;
+
+	private void Awake()
+	{
+		m_currency.OnCurencyChanged += OnCurencyChanged;
+		m_name.text = m_currency.name;
+		OnCurencyChanged();
+	}
+
+	private void OnDestroy()
+	{
+		m_currency.OnCurencyChanged -= OnCurencyChanged;
+	}
+
+	private void OnCurencyChanged()
+	{
+		m_amount.text = m_currency.Amount.ToString("0");
+	}
+}
