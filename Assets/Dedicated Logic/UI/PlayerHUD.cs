0b5c9868ce34364d86683070c39ece0849d40440
Łukasz <ukidabek@gmail.com>
2023-02-20 20:02:27 +0100

    [Code] SkillHolderReferenceHost added.

--- Assets/Dedicated Logic/UI/PlayerHUD.cs
+++ Assets/Dedicated Logic/UI/PlayerHUD.cs
@@ -10,7 +10,7 @@ public class PlayerHUD : MonoBehaviour
     [SerializeField] private ResourceSliderDisplayModel[] m_resourceDisplays = null;
 	[SerializeField] private CastSliderDisplayModel m_castSliderDisplay = null;
 	[Space]
-	[SerializeField] private SkillHolder m_skillHolder = null;
+	[SerializeField] private SkillHolderReferenceHost m_skillHolderReferenceHost = null;
 	[SerializeField] private SkillSlotDisplay[] m_skillSlotDisplays = null;
 
 	private void Start()
@@ -27,10 +27,11 @@ public class PlayerHUD : MonoBehaviour
 		foreach (var item in list)
 			item.Initialize();
 
+		var skillHolder = m_skillHolderReferenceHost.Instance;
 		var count = m_skillSlotDisplays.Length;
 		for (var i = 0; i < count; i++)
 		{
-			var skillSlot = m_skillHolder.GetSkillSlot(i);
+			var skillSlot = skillHolder.GetSkillSlot(i);
 			m_skillSlotDisplays[i].Initialize(skillSlot);
 		}
 	}
