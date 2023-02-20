2a9e82650e34004ee2c9497764fa9d1b4f1f2e31
Łukasz <ukidabek@gmail.com>
2023-02-20 15:32:37 +0100

    [Code] Spawn handling state logic added.

---
+++ Assets/Dedicated Logic/States/SpawnTriggerStateLogic.cs
@@ -0,0 +1,17 @@
+﻿using Shlashurai.Containers;
+using UnityEngine;
+using Utilities.States;
+
+namespace Shlashurai.Enemy.Logic
+{
+	public class SpawnTriggerStateLogic : StateLogic
+    {
+        [SerializeField] private ItemSpawnManager m_spawn = null;
+
+		public override void Activate()
+		{
+			base.Activate();
+            m_spawn.Spawn();
+		}
+	}
+}
