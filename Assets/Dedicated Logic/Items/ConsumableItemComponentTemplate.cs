1a83c22f9e6f08aa0e3c075598de3c9af48cd691
Łukasz <ukidabek@gmail.com>
2023-02-20 11:38:18 +0100

    [Code] ConsumableItem converted to consumable item component. Item class refactored to handle interface change.

---
+++ Assets/Dedicated Logic/Items/ConsumableItemComponentTemplate.cs
@@ -0,0 +1,11 @@
+﻿using Shlashurai.Consumable;
+using Shlashurai.Items;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ConsumableItemComponentTemplate", menuName = "Items/Components/ConsumableItemComponentTemplate")]
+public class ConsumableItemComponentTemplate : ItemComponentTemplate
+{
+	[SerializeField] private ConsumableEffect[] m_consumableEffects;
+
+	public override IItemComponent Create() => new ConsumableItemComponent(m_consumableEffects);
+}
