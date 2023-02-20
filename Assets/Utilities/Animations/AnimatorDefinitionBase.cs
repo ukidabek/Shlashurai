b11f2169908c9749f760e0479ce5d342847a9c3b
Łukasz <ukidabek@gmail.com>
2023-02-20 15:46:25 +0100

    [Code] Compilation error fixes.

--- Assets/Utilities/Animations/AnimatorDefinitionBase.cs
+++ Assets/Utilities/Animations/AnimatorDefinitionBase.cs
@@ -1,10 +1,15 @@
-﻿using UnityEditor.Animations;
+﻿#if UNITY_EDITOR
+using UnityEditor.Animations;
+#endif
+
 using UnityEngine;
 
 namespace Utilities.General
 {
     public class AnimatorDefinitionBase : ScriptableObject
     {
+#if UNITY_EDITOR
         [SerializeField, HideInInspector] protected AnimatorController _animator = null;
+#endif
     }
 }
