14649a87379e93a3cb70a076ea937adcaf03503b
Łukasz <ukidabek@gmail.com>
2023-02-20 15:32:07 +0100

    [Code] DeathStateLogic renamed.

--- Assets/Dedicated Logic/States/DeathStateLogic.cs
+++ Assets/Dedicated Logic/States/DeathStateLogic.cs
@@ -4,7 +4,7 @@ using Utilities.States;
 
 namespace Shlashurai.Enemy.Logic
 {
-    public class EnemyDeathState : CoroutineStateLogicMonoBehaviour
+	public class DeathStateLogic : CoroutineStateLogic
     {
         [SerializeField] private float m_timeToDeactivate = 1f;
         [SerializeField] private GameObject m_objectToUnderactive = null; 
