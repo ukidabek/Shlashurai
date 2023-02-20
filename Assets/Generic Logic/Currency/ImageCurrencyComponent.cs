ca73957df985a99003f01c09fbda3e1a90ac9ecd
Łukasz <ukidabek@gmail.com>
2023-02-20 13:30:49 +0100

    [Code] Currency logic added.

---
+++ Assets/Generic Logic/Currency/ImageCurrencyComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Money
+{
+	[CreateAssetMenu(fileName = "ImageCurrencyComponent", menuName = "Currency/Component/ImageCurrencyComponent")]
+	public class ImageCurrencyComponent : CurrencyComponent
+	{
+		[SerializeField] private Sprite m_image = null;
+		public Sprite Image => m_image;
+	}
+}
