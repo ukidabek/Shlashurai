a1bf19315b0adb75def0c1fc980350c00ebfe6ff
Łukasz <ukidabek@gmail.com>
2023-02-22 18:15:53 +0100

    [Code] StatusHandlers for GameObject and components added. StateID, and StateDictionary (for entering state by ID) added. SubStateMachine refactored and simplified.

--- Assets/Utilities/States/State.cs
+++ Assets/Utilities/States/State.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 
 namespace Utilities.States
 {
-    public class State : MonoBehaviour, IState
+
+	public class State : MonoBehaviour, IState
     {
+        [SerializeField] private StateID m_stateID;
+		public IStateID ID => m_stateID;
+
+
         [SerializeField] private Object[] m_logic = null;
         public IEnumerable<IStateLogic> Logic => m_logic.OfType<IStateLogic>();
 
-        public void Enter()
+		public void Enter()
         {
             foreach (var stateLogic in Logic) 
                 stateLogic.Activate();
