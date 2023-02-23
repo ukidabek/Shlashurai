dbc3dbe0375c5435b1d30fe92152b06f4e803dd8
Łukasz <ukidabek@gmail.com>
2023-02-23 09:27:47 +0100

    [Code] Pause handling logic added.

---
+++ Assets/Generic Logic/GameManager/PauseStateLogic.cs
@@ -0,0 +1,21 @@
+﻿using Shlashurai.Player.Input;
+using UnityEngine;
+using Utilities.States;
+
+public class PauseStateLogic : StateLogic, ISwitchStateCondition
+{
+	[SerializeField] private InputValues m_inputValues = null;
+
+	public bool Condition
+	{
+		get
+		{
+			if(m_inputValues.Pause)
+			{
+				m_inputValues.Pause = false;
+				return true;
+			}
+			return false;
+		}
+	}
+}
