2175bb97eb9d3eb0aa62e644b4acb9856af2cc48
Łukasz <ukidabek@gmail.com>
2023-02-24 09:53:56 +0100

    [Code] Waves spawn manager added.

--- Assets/Dedicated Logic/Spawn/RandomEnemySpawnManager.cs
+++ Assets/Dedicated Logic/Spawn/RandomEnemySpawnManager.cs
@@ -1,34 +1,13 @@
-﻿using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 using Utilities.ReferenceHost;
 using System.Collections;
-using System;
 
 namespace Shlashurai.Spawn
 {
+
 	public class RandomEnemySpawnManager : MonoBehaviour
 	{
-		[Serializable]
-		public class Spawn
-		{
-			[SerializeField] private GameObjectPoolHander m_handler = null;
-
-			public GameObject ObjectToSpawn => m_handler.ObjectToSpawn;
-
-			[SerializeField] private float m_chance = 1.0f;
-			public float Chance => m_chance;
-			private Vector2 m_range = Vector2.zero;
-
-			public void Initialize(float previousChance)
-			{
-				//base.Initialize(TODO);
-				m_range.Set(previousChance, previousChance + m_chance);
-			}
-
-			public bool IsInRange(float roll) => m_range.x <= roll && m_range.y >= roll;
-
-		}
 
 		[SerializeField] private TransformReferenceHost m_playerTransform = null;
 		[SerializeField] private GameObjectSpawn m_spawn = null;
@@ -42,13 +21,7 @@ namespace Shlashurai.Spawn
 
 		private void Awake()
 		{
-			m_totalChance = m_enemyToSpawn.Sum(x => x.Chance);
-			var previousChance = 0f;
-			foreach (var item in m_enemyToSpawn)
-			{
-				item.Initialize(previousChance);
-				previousChance = item.Chance;
-			}
+			m_totalChance = Spawn.Initialize(m_enemyToSpawn);
 		}
 
 		private IEnumerator SpawnCoroutine(GameObject enemy)
@@ -79,7 +52,7 @@ namespace Shlashurai.Spawn
 				m_counter = m_spawnInterval;
 				var roll = UnityEngine.Random.Range(0f, m_totalChance);
 
-				var objectSpawner = m_enemyToSpawn.FirstOrDefault(x => x.IsInRange(roll));
+				var objectSpawner = Spawn.GetRandom(m_enemyToSpawn, roll);
 				if (objectSpawner == null)
 					return;
 
