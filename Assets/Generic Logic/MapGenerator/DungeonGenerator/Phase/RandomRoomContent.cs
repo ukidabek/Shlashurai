914d493195e1b863e8255ca867a5882b00fa6bd0
Łukasz <ukidabek@gmail.com>
2023-02-24 09:53:08 +0100

    [Code] Management of random room content added. Runtime NavMesh baking logic added.

---
+++ Assets/Generic Logic/MapGenerator/DungeonGenerator/Phase/RandomRoomContent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MapGeneration.DungeonGenerator.V3
+{
+	public class RandomRoomContent : MonoBehaviour
+	{
+		[SerializeField, Range(0f,1f)] private float m_chance = .2f;
+		
+		public void Randomize()
+		{
+			var roll = Random.Range(0f, 1f);
+			gameObject.SetActive(roll <= m_chance);
+		}
+
+		public void ForceOff() => gameObject.SetActive(false);
+	}
+}
