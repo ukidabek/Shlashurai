840419a81ebb2f0f8e976bd39b1d1d1d174bbd4f
Łukasz <ukidabek@gmail.com>
2023-03-01 13:51:13 +0100

    [Code] InventoryDisplay logic added.

---
+++ Assets/Dedicated Logic/Items/ItemPrefabComponentHandler.cs
@@ -0,0 +1,13 @@
+﻿using Shlashurai.Items;
+using UnityEngine;
+
+public class ItemPrefabComponentHandler : ItemComponentHandler<ItemPrefabComponent>
+{
+	public override void Handle(IItemComponent itemComponent)
+	{
+		var itemPrefabComponent = (ItemPrefabComponent)itemComponent;
+		itemPrefabComponent.SetActive(false);
+		itemPrefabComponent.SetParent(transform, false);
+		itemPrefabComponent.Instance.transform.localPosition = Vector3.zero;
+	}
+}
---
+++ Assets/Dedicated Logic/UI/InventorySlotDisplay.cs
@@ -0,0 +1,13 @@
+﻿using Shlashurai.Items;
+using TMPro;
+using UnityEngine;
+
+public class InventorySlotDisplay : MonoBehaviour
+{
+    [SerializeField] private TMP_Text m_itemName = null;
+
+	internal void Initialize(IItemSlot slot)
+	{
+		m_itemName.text = slot.Item.DisplayName;
+	}
+}
