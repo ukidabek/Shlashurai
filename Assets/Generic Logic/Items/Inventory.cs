d9555c397316f624e172ad78be351a7a148b9397
Łukasz <ukidabek@gmail.com>
2023-03-01 13:50:51 +0100

    [Code] Inventory logic added.

---
+++ Assets/Generic Logic/Items/Inventory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shlashurai.Items
+{
+	public class Inventory : IInventory
+	{
+		private int m_maxSlotCount = -1;
+		private List<ItemSlot> m_slots = new List<ItemSlot>();
+
+		public IEnumerable<IItemSlot> Slots => m_slots;
+
+		private IEnumerable<IItemComponentHandler> m_onAddComponentsHandlers = null;
+		private IEnumerable<IItemComponentHandler> m_onRemoveComponentsHandlers = null;
+
+		public event Action OnInventoryChanged;
+
+		public Inventory(int maxSlotCount, 
+			IEnumerable<IItemComponentHandler> onAddComponentsHandlers,
+			IEnumerable<IItemComponentHandler> onRemoveComponentsHandlers)
+		{
+			m_maxSlotCount = maxSlotCount;
+			m_onAddComponentsHandlers = onAddComponentsHandlers;
+			m_onRemoveComponentsHandlers = onRemoveComponentsHandlers;
+		}
+
+		public bool AddItem(IItem item)
+		{
+			if (m_maxSlotCount >= 0 && m_slots.Count == m_maxSlotCount)
+				return false;
+
+			ItemSlot slot = null;
+			if (item.IsStackable)
+			{
+				slot = m_slots.FirstOrDefault(slot => slot.Item.ID == item.ID);
+			}
+
+			if (slot == null)
+			{
+				slot = new ItemSlot() {
+					Item = item,
+				};
+
+				m_slots.Add(slot);
+			}
+
+			++slot.Count;
+
+			foreach (var component in item.Components)
+			{
+				IItemComponentHandler componentHandler = GetHandler(m_onAddComponentsHandlers, component);
+				componentHandler?.Handle(component);
+			}
+
+			OnInventoryChanged?.Invoke();
+			return true;
+		}
+
+		public void RemoveItem(IItem item)
+		{
+			var slot = m_slots.FirstOrDefault(itemSlot => itemSlot.Item == item);
+			--slot.Count;
+			if (slot.Count <= 0)
+				m_slots.Remove(slot);
+			OnInventoryChanged?.Invoke();
+		}
+
+		private IItemComponentHandler GetHandler(IEnumerable<IItemComponentHandler> handlers, IItemComponent component) 
+			=> handlers.FirstOrDefault(handler => handler.CanHandle(component));
+	}
+}
---
+++ Assets/Generic Logic/Items/InventoryManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shlashurai.Items
+{
+	public class InventoryManager : MonoBehaviour, IInventory
+	{
+		private Inventory m_inventory = null;
+
+		[SerializeField] private int m_maxSlotCount = -1;
+		[SerializeField] private ItemComponentHandler[] m_onAddComponentsHandlers = null;
+		[SerializeField] private ItemComponentHandler[] m_onRemoveComponentsHandlers = null;
+
+		public event Action OnInventoryChanged;
+
+		private void Awake()
+		{
+			m_inventory = new Inventory(
+				m_maxSlotCount, 
+				m_onAddComponentsHandlers,
+				m_onRemoveComponentsHandlers);
+
+			m_inventory.OnInventoryChanged += InvokeOnChangeEvent;
+		}
+
+		private void InvokeOnChangeEvent() => OnInventoryChanged?.Invoke();
+
+		private void OnDestroy()
+		{
+			m_inventory.OnInventoryChanged -= InvokeOnChangeEvent;
+		}
+
+		public IEnumerable<IItemSlot> Slots => m_inventory.Slots;
+
+		public bool AddItem(IItem item) => m_inventory.AddItem(item);
+
+		public void RemoveItem(IItem item) => m_inventory.RemoveItem(item);
+	}
+}
