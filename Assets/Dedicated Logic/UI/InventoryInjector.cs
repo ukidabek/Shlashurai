00554478b19db2adaa2696c9c7f4ce25c6c68bb2
Łukasz <ukidabek@gmail.com>
2023-03-01 13:51:13 +0100

    [Code] InventoryDisplay logic added.

---
+++ Assets/Dedicated Logic/UI/InventoryInjector.cs
@@ -0,0 +1,18 @@
+﻿using Shlashurai.Player;
+using UnityEngine;
+
+public class InventoryInjector : MonoBehaviour
+{
+	[SerializeField] private InventoryManagerReferenceHost m_inventoryManger = null;
+	[SerializeField] private InventoryDisplay m_inventoryDisplay = null;
+
+	private void Awake()
+	{
+		m_inventoryManger.OnReferenceChanged += OnReferenceChanged;
+	}
+
+	private void OnReferenceChanged()
+	{
+		m_inventoryDisplay.Initialize(m_inventoryManger.Instance);
+	}
+}
