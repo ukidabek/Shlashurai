4023a59fc681603d4507992bce39c669415e5c32
Łukasz <ukidabek@gmail.com>
2023-03-06 14:50:40 +0100

    [Code] Item logic moved to dedicated folder.

--- Assets/Generic Logic/Items/Item/ItemHelperClass.cs
+++ Assets/Generic Logic/Items/Item/ItemHelperClass.cs
@@ -7,6 +7,8 @@ namespace Shlashurai.Items
 	{
 		public static T GetComponent<T>(this IItem item) => item.Components.OfType<T>().FirstOrDefault();
 
+		public static bool HasComponent<T>(this IItem item) => item.Components.OfType<T>().Any();
+
 		public static IEnumerable<T> GetComponentsOfType<T>(this IItem item) => item.Components.OfType<T>();
 
 		public static void SetItemStatus(this IItem item, bool status) => item
