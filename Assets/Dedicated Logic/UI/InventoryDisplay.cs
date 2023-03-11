64294eadead669ad8c726a43f8ffdb46f3eaebc2
Łukasz <ukidabek@gmail.com>
2023-03-11 11:14:32 +0100

    [Code] Inventory UI updated to handle equipped items.

--- Assets/Dedicated Logic/UI/InventoryDisplay.cs
+++ Assets/Dedicated Logic/UI/InventoryDisplay.cs
@@ -3,33 +3,24 @@ using Shlashurai.Player;
 using UnityEngine;
 using Utilities.Pool;
 
-public class InventoryDisplay : MonoBehaviour
+public class InventoryDisplay : Display<InventoryManagerReferenceHost, InventoryManager>
 {
 	[SerializeField] private InventorySlotDisplay m_inventorySlotPrefab = null;
 	[SerializeField] private Transform m_slotDisplayParent = null;
-	[SerializeField] private ItemDescriptionDisplay m_itemDescriptionDisplay = null;
-	[SerializeField] private InventoryManagerReferenceHost m_inventoryManagerReferenceHost = null;
+	[SerializeField] private ItemDisplay m_itemDescriptionDisplay = null;
 	[SerializeField] private InventoryDisplayButtonHandler[] m_buttonHandlers = null;
 
 	private ComponentPool<InventorySlotDisplay> m_inventorySlotDisplayPool = null;
 	private IInventory m_inventory = null;
 	private IItem m_item = null;
 
-	private void Awake()
+	protected override void OnDestroy()
 	{
-		m_inventoryManagerReferenceHost.OnReferenceChanged += OnReferenceChanged;
-		if(m_inventoryManagerReferenceHost.Instance != null)
-			Initialize(m_inventoryManagerReferenceHost.Instance);
-	}
-
-	private void OnDestroy()
-	{
-		m_inventoryManagerReferenceHost.OnReferenceChanged -= OnReferenceChanged;
+		base.OnDestroy();
 		if (m_inventory == null) return;
 		m_inventory.OnItemAdded -= OnItemAddedCallback;
 		m_inventory.OnItemRemoved -= OnItemRemovedCallback;
 	}
-
 
 	private void OnItemAddedCallback(IItemSlot obj) => OnInventoryChanged();
 
@@ -38,23 +29,6 @@ public class InventoryDisplay : MonoBehaviour
 		OnInventoryChanged();
 		if(obj.Item == m_item && obj.Count == 0)
 			m_itemDescriptionDisplay.ClearHandlers();
-	}
-
-	private void OnReferenceChanged() => Initialize(m_inventoryManagerReferenceHost.Instance);
-
-	public void Initialize(IInventory inventory)
-	{
-		if (m_inventorySlotDisplayPool == null)
-			m_inventorySlotDisplayPool = new ComponentPool<InventorySlotDisplay>(m_inventorySlotPrefab, m_slotDisplayParent, 20);
-
-		m_inventory = inventory;
-		m_inventory.OnItemAdded += OnItemAddedCallback;
-		m_inventory.OnItemRemoved += OnItemRemovedCallback;
-
-		foreach (var buttonHandler in m_buttonHandlers)
-			buttonHandler.Initialize(m_inventory);
-
-		OnInventoryChanged();
 	}
 
 	private void OnInventoryChanged()
@@ -80,5 +54,20 @@ public class InventoryDisplay : MonoBehaviour
 		foreach (var buttonHandler in m_buttonHandlers)
 			buttonHandler.SetItem(item);
 		m_itemDescriptionDisplay.Initialize(item);
+	}
+
+	protected override void Initialize(InventoryManager instance)
+	{
+		if (m_inventorySlotDisplayPool == null)
+			m_inventorySlotDisplayPool = new ComponentPool<InventorySlotDisplay>(m_inventorySlotPrefab, m_slotDisplayParent, 20);
+
+		m_inventory = instance;
+		m_inventory.OnItemAdded += OnItemAddedCallback;
+		m_inventory.OnItemRemoved += OnItemRemovedCallback;
+
+		foreach (var buttonHandler in m_buttonHandlers)
+			buttonHandler.Initialize(m_inventory);
+
+		OnInventoryChanged();
 	}
 }
