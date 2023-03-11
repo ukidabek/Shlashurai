bb76f6e9351d8bde32c222dd37751c7b03ca0aca
Łukasz <ukidabek@gmail.com>
2023-03-11 11:14:32 +0100

    [Code] Inventory UI updated to handle equipped items.

---
+++ Assets/Dedicated Logic/UI/EquipButtonHandler.cs
@@ -0,0 +1,24 @@
+﻿using Shlashurai.Items;
+using Shlashurai.Player;
+using UnityEngine;
+
+public class EquipButtonHandler : InventoryDisplayButtonHandler
+{
+	[SerializeField] private EquipmentManagerReferenceHost m_equipmentManagerReferenceHost = null;
+	
+	public override void SetItem(IItem item)
+	{
+		base.SetItem(item);
+
+		if (item == null) return;
+
+		var isEquipable = item.HasComponent<IEquipable>();
+		m_button.gameObject.SetActive(isEquipable);
+	}
+
+	protected override void OnClick()
+	{
+		var equipmentManager = m_equipmentManagerReferenceHost.Instance;
+		equipmentManager.Equip(m_item, true);
+	}
+}
---
+++ Assets/Dedicated Logic/UI/EquipmentDisplay.cs
@@ -0,0 +1,19 @@
+﻿using Shlashurai.Items;
+using Shlashurai.Player;
+using System.Linq;
+using UnityEngine;
+
+public class EquipmentDisplay : Display<EquipmentManagerReferenceHost, EquipmentManager>
+{
+	[SerializeField] private EquipmentSlotDisplay[] m_itemDisplays = null;
+
+	protected override void Initialize(EquipmentManager instance)
+	{
+		var slots = instance.Slots;
+		foreach (var slot in slots) 
+		{
+			var display = m_itemDisplays.FirstOrDefault(display => display.Descriptors.Intersect(slot.Descriptors).Any());
+			display?.Initialize(slot);
+		}
+	}
+}
