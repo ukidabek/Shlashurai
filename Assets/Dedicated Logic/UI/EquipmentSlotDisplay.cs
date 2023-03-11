44c9784547c32df26a91edbea5c6400bdad28282
Łukasz <ukidabek@gmail.com>
2023-03-11 11:14:32 +0100

    [Code] Inventory UI updated to handle equipped items.

---
+++ Assets/Dedicated Logic/UI/EquipmentSlotDisplay.cs
@@ -0,0 +1,31 @@
+﻿using Shlashurai.Items;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentSlotDisplay : MonoBehaviour
+{
+	[SerializeField] private ItemDisplay m_itemDisplay;
+
+	[SerializeField] private EquipmentDescriptor[] m_descriptors = null;
+
+	private IEquipmentSlot m_slot = null;
+
+	public IEnumerable<IEquipmentDescriptor> Descriptors => m_descriptors;
+
+	public void Initialize(IEquipmentSlot slot)
+	{
+		if(m_slot != null)
+			m_slot.OnItemChanged -= ManageDisplay;
+
+		m_slot = slot;
+
+		m_slot.OnItemChanged += ManageDisplay;
+		if (m_slot.Item != null)
+			ManageDisplay();
+	}
+
+	private void ManageDisplay()
+	{
+		m_itemDisplay.Initialize(m_slot.Item);
+	}
+}
