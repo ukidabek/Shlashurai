5825217ce24bd8b1ee2c477ef0d5ed93e75c4a62
Łukasz <ukidabek@gmail.com>
2023-03-12 15:40:20 +0100

    [Code] Armor handling logic added.

---
+++ Assets/Dedicated Logic/Items/ArmorEquipmentEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Shlashurai.Items
+{
+	public class ArmorEquipmentEventHandler : EquipmentEventHandler
+	{
+		[SerializeField] private Damagable m_damagable = null;
+
+		protected override void OnItemEquipped(IItem item)
+		{
+			if(item == null) return;
+			var armor = item.GetComponent<IArmor>();
+			if (armor == null) return;
+			m_damagable.AddArmor(armor);
+		}
+
+		protected override void OnItemUnequipped(IItem item)
+		{
+			if (item == null) return;
+			var armor = item.GetComponent<IArmor>();
+			if (armor == null) return;
+			m_damagable.RemoveArmor(armor);
+		}
+	}
+}
