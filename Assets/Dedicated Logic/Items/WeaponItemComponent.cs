c0ea47eb031aea2512cbd40a709f0ce2b88919f9
Łukasz <ukidabek@gmail.com>
2023-03-12 13:10:41 +0100

    [Code] Damage modification by weapon equipping added.

---
+++ Assets/Dedicated Logic/Items/WeaponItemComponent.cs
@@ -0,0 +1,15 @@
+﻿using Shlashurai.Items;
+
+public class WeaponItemComponent : IItemComponent
+{
+	public float MinDamage { get; }
+	public float MaxDamage { get; }
+
+	public WeaponItemComponent(float minDamage, float maxDamage)
+	{
+		MinDamage = minDamage;
+		MaxDamage = maxDamage;
+	}
+
+	public float GetDamage() => UnityEngine.Random.Range(MinDamage, MaxDamage);
+}
