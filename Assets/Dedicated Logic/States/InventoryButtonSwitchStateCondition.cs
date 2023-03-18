23de64c3aa92b6b3c6a5e854dedd3bbee34520f0
Łukasz <ukidabek@gmail.com>
2023-03-18 10:16:10 +0100

    [Code] Logic for handling inventory button added.

---
+++ Assets/Dedicated Logic/States/InventoryButtonSwitchStateCondition.cs
@@ -0,0 +1,28 @@
+﻿using Shlashurai.Player.Input;
+using UnityEngine;
+using Utilities.States;
+
+namespace Shlashurai.Player.Logic
+{
+	public class InventoryButtonSwitchStateCondition : StateLogic, ISwitchStateCondition
+	{
+		[SerializeField] private InputValues m_inputValues = null;
+		public bool Condition
+		{
+			get
+			{
+				if(m_toggle == m_inputValues.Inventory)
+					m_toggle = true;
+				return m_toggle && m_inputValues.Inventory;
+			}
+		}
+
+		private bool m_toggle = false;
+
+		public override void Activate()
+		{
+			base.Activate();
+			m_toggle = false;
+		}
+	}
+}
