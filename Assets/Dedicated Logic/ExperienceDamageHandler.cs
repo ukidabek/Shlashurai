d6760468a6f6b266594226e7c6a6c79471ff6835
Łukasz <ukidabek@gmail.com>
2023-03-19 13:29:25 +0100

    [Code] Experience add handling logic added.

---
+++ Assets/Dedicated Logic/ExperienceDamageHandler.cs
@@ -0,0 +1,31 @@
+﻿using Progress;
+using Shlashurai.Characters;
+using UnityEngine;
+using Weapons;
+
+public class ExperienceDamageHandler : DamageHandler
+{
+	[SerializeField] private ResourceHandler m_resourceChandler = null;
+	[SerializeField] protected float m_experience = 10f;
+
+	private bool m_experienceGranted = false;
+
+	private void OnEnable()
+	{
+		m_experienceGranted = false;
+	}
+
+	public override void OnDamage(IDamage damage)
+	{
+		if (m_experienceGranted ||
+			m_resourceChandler.Value > 0)
+			return;
+
+		m_experienceGranted = true;
+		
+		var progressManager = damage.DamagingObject.GetComponent<ProgressManager>();
+		
+		if (progressManager == null) return;
+		progressManager.AddExperience(m_experience);
+	}
+}
---
+++ Assets/Dedicated Logic/HealthDamageHandler.cs
@@ -0,0 +1,45 @@
+﻿using Shlashurai.Characters;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Weapons;
+
+public class HealthDamageHandler : DamageHandler
+{
+	private List<IArmor> m_armors = new List<IArmor>();
+
+	[SerializeField] private float m_armor = 0f;
+	public float Armor
+	{
+		get => m_armor;
+		set => m_armor = value;
+	}
+
+	[SerializeField] private float m_additionalArmor = 0;
+
+	[SerializeField] private ResourceHandler m_resourceChandler = null;
+
+	public override void OnDamage(IDamage damage)
+	{
+		var damageAmount = damage.Amount - (m_armor + m_additionalArmor);
+		if (damageAmount <= 0f) return;
+
+		m_resourceChandler.Value -= damageAmount;
+	}
+
+	public void AddArmor(IArmor armor)
+	{
+		if (m_armors.Contains(armor)) return;
+		m_armors.Add(armor);
+		CalculateAdditionalArmor();
+	}
+
+	public void RemoveArmor(IArmor armor)
+	{
+		if (m_armors.Contains(armor)) return;
+		m_armors.Remove(armor);
+		CalculateAdditionalArmor();
+	}
+
+	private void CalculateAdditionalArmor() => m_additionalArmor = m_armors.Sum(armor => armor.Value);
+}
