ae1fbde0bed78cb94de40edd791c52ee9f74d287
Łukasz <ukidabek@gmail.com>
2023-03-19 13:28:46 +0100

    [Code] Character progress handling logic added.

---
+++ Assets/Generic Logic/Progress/CurveProgressDefinition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Progress
+{
+	[CreateAssetMenu(fileName = "CurveProgressDefinition", menuName = "Progress/CurveProgressDefinition")]
+	public class CurveProgressDefinition : ProgressDefinition
+	{
+		[SerializeField] private AnimationCurve m_levelCurve = new AnimationCurve();
+		public override int Evaluate(float experience, int currentLevel)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
