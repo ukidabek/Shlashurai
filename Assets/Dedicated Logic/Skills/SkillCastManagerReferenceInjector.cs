0b67a42a6a9afb50219db404408c6304e09092a8
Łukasz <ukidabek@gmail.com>
2023-03-19 11:16:42 +0100

    [Code] Reference injectors added.

---
+++ Assets/Dedicated Logic/Skills/SkillCastManagerReferenceInjector.cs
@@ -0,0 +1,6 @@
+﻿using Shlashurai.Skill;
+using Utilities.ReferenceHost;
+
+public class SkillCastManagerReferenceInjector : ReferenceHostInjector<SkillCastManagerReferenceHost, SkillCastManager>
+{
+}
