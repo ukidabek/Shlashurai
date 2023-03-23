268802a33cdc8044ef809da4014e50b493bff50f
Łukasz <ukidabek@gmail.com>
2023-03-23 19:39:41 +0100

    [Code] Experience related display logic added.

---
+++ Assets/Dedicated Logic/UI/ExperienceProgressDisplay.cs
@@ -0,0 +1,38 @@
+﻿using Progress;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilities.ReferenceHost;
+
+public class ExperienceProgressDisplay : MonoBehaviour
+{
+	[Inject] private IProgressManager m_progressManager = null;
+	[SerializeField] private Slider m_experienceProgress = null;
+
+	public void Initialize()
+	{
+		if (m_progressManager == null) return;
+		m_progressManager.OnExperienceAdded += OnExperienceAdded;
+		m_progressManager.OnLevelChanged += OnExperienceAdded;
+		OnExperienceAdded();
+	}
+
+	private void OnDestroy()
+	{
+		if (m_progressManager == null) return;
+		m_progressManager.OnExperienceAdded -= OnExperienceAdded;
+		m_progressManager.OnLevelChanged -= OnExperienceAdded;
+	}
+
+	private void OnExperienceAdded(int obj = 0)
+	{
+		var currentLevelExperience = m_progressManager.CurrentLevelExperience;
+		var nextLevelExperience = m_progressManager.NextLevelExperience;
+
+		var experienceDoGet = (nextLevelExperience - currentLevelExperience);
+		var experienceDelta = Math.Abs(currentLevelExperience - m_progressManager.Experience);
+		var experienceCollectingProgress = experienceDelta / (float)experienceDoGet;
+
+		m_experienceProgress.value = experienceCollectingProgress;
+	}
+}
