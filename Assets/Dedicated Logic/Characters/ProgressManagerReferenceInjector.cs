e95f6203b922a4764842575077a44804b3f07f16
Łukasz <ukidabek@gmail.com>
2023-03-23 19:40:41 +0100

    [Code] Progress Manager reference setters added.

---
+++ Assets/Dedicated Logic/Characters/ProgressManagerReferenceInjector.cs
@@ -0,0 +1,9 @@
+﻿using Progress;
+using Utilities.ReferenceHost;
+
+namespace Shlashurai.Player
+{
+	public class ProgressManagerReferenceInjector : ReferenceHostInjector<ProgressManagerReferenceHost, ProgressManager, IProgressManager>
+	{
+	}
+}
