c7619268eaa130c0bdc508b16d44ccf7ef7f03d9
Łukasz <ukidabek@gmail.com>
2023-03-23 19:40:41 +0100

    [Code] Progress Manager reference setters added.

--- Assets/Dedicated Logic/Characters/EquipmentManagerReferenceHostSetter.cs
+++ Assets/Dedicated Logic/Characters/EquipmentManagerReferenceHostSetter.cs
@@ -4,6 +4,6 @@ using Utilities.ReferenceHost;
 namespace Shlashurai.Player
 {
 	public class EquipmentManagerReferenceHostSetter : ReferenceHostSetter<EquipmentManagerReferenceHost, EquipmentManager>
-	{ 
+	{
 	}
 }
---
+++ Assets/Dedicated Logic/Characters/ProgressManagerReferenceHost.cs
@@ -0,0 +1,11 @@
+﻿using Progress;
+using UnityEngine;
+using Utilities.ReferenceHost;
+
+namespace Shlashurai.Player
+{
+	[CreateAssetMenu(fileName = "ProgressManagerReferenceHost", menuName = "ReferenceHost/ProgressManagerReferenceHost")]
+	public class ProgressManagerReferenceHost : ReferenceHost<ProgressManager>
+	{
+	}
+}
