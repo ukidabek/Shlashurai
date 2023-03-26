e1278e00112244da41d614ca0948c17b7f615ba5
Łukasz <ukidabek@gmail.com>
2023-03-26 09:57:23 +0200

    [Code] Some StateDictionary, SwitchStateLogic editors improvements.

--- Assets/Utilities/States/Editor/SwitchStateStateLogicEditor.cs
+++ Assets/Utilities/States/Editor/SwitchStateStateLogicEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 
 namespace Utilities.States
@@ -6,12 +7,37 @@ namespace Utilities.States
 	[CustomEditor(typeof(SwitchStateStateLogic))]
 	public class SwitchStateStateLogicEditor : Editor
 	{
+		private SwitchStateStateLogic m_switchStateStateLogic = null;
+		FieldInfo m_stateFieldInfo = null;
+
+		private void OnEnable()
+		{
+			m_switchStateStateLogic = (target as SwitchStateStateLogic);
+			m_stateFieldInfo = m_switchStateStateLogic
+				.GetType()
+				.GetField("_stateToEnter", BindingFlags.Instance | BindingFlags.NonPublic);
+		}
+
 		public override void OnInspectorGUI()
 		{
+			var state = m_stateFieldInfo.GetValue(target) as State;
+			if (state != null) 
+			{
+				var oldColor = GUI.color;
+				GUI.color = Color.yellow;
+				GUILayout.Label(state.name);
+				GUI.color = oldColor;
+			}
+
 			base.OnInspectorGUI();
 			if (GUILayout.Button("Get StateMachine"))
 			{
-				(target as SwitchStateStateLogic).GetStateMachineObject();
+				m_switchStateStateLogic.GetStateMachineObject();
+				EditorUtility.SetDirty(target);
+			}
+			if (GUILayout.Button("Get Conditions"))
+			{
+				m_switchStateStateLogic.GetConditions();
 				EditorUtility.SetDirty(target);
 			}
 		}
