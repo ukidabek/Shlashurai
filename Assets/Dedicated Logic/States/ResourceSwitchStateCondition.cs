2729c0baa50d84470d69f52155c77781046ba680
Łukasz <ukidabek@gmail.com>
2023-03-26 12:29:32 +0200

    [Code] ResourceSwitchStateCondition updated to be more generic.

--- Assets/Dedicated Logic/States/ResourceSwitchStateCondition.cs
+++ Assets/Dedicated Logic/States/ResourceSwitchStateCondition.cs
@@ -6,10 +6,36 @@ namespace Shlashurai.Character
 {
 	public class ResourceSwitchStateCondition : SwitchStateConditionBase
 	{
+		public enum Mode
+		{
+			Less,
+			LessOrEqual,
+			Greater,
+			GreaterOrEqual
+		}
+
 		[SerializeField] private ResourceHandler m_resourceChandler = null;
+		[SerializeField] private float m_value = 10f;
+		[SerializeField] private Mode m_mode = Mode.LessOrEqual;
 
-		[SerializeField] private float m_minimumValue = 10f;
-
-		public override bool Condition => m_resourceChandler.Value >= m_minimumValue;
+		public override bool Condition
+		{
+			get
+			{
+				switch (m_mode)
+				{
+					case Mode.Less:
+						return m_resourceChandler.Value < m_value;
+					case Mode.LessOrEqual:
+						return m_resourceChandler.Value <= m_value;
+					case Mode.Greater:
+						return m_resourceChandler.Value > m_value;
+					case Mode.GreaterOrEqual:
+						return m_resourceChandler.Value >= m_value;
+					default:
+						return false;
+				}
+			}
+		}
 	}
 }
