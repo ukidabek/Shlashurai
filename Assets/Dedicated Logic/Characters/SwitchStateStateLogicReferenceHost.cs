4d8fdf6cbd2bce2e0f0056dd75a7ec5385cc9324
Łukasz <ukidabek@gmail.com>
2023-03-26 10:00:24 +0200

    [Code] End game loop condition added.

---
+++ Assets/Dedicated Logic/Characters/SwitchStateStateLogicReferenceHost.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using Utilities.ReferenceHost;
+using Utilities.States;
+
+[CreateAssetMenu(fileName = "SwitchStateStateLogic", menuName = "ReferenceHost/SwitchStateStateLogic")]
+public class SwitchStateStateLogicReferenceHost : ReferenceHost<SwitchStateStateLogic>
+{
+}
