d1d2dbad0521096e81adbff710aee569e4be6768
Łukasz <ukidabek@gmail.com>
2023-03-26 10:00:01 +0200

    [Code] End screen logic added.

---
+++ Assets/Generic Logic/GameManager/DelayStateLogicCondition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using Utilities.General;
+using Utilities.States;
+
+public class DelayStateLogicCondition : StateLogic, ISwitchStateCondition
+{
+	[SerializeField] private bool m_condition = false;
+	public bool Condition => m_condition;
+
+	[SerializeField] private float m_delay = 3f;
+
+	private CoroutineManager m_coroutineManager = null;
+
+	public override void Activate()
+	{
+		base.Activate();
+		m_condition = false;
+
+		if (m_coroutineManager == null)
+			m_coroutineManager = new CoroutineManager(this, DelayCoroutine());
+
+		m_coroutineManager.Run();
+	}
+
+	private IEnumerator DelayCoroutine()
+	{
+		yield return new WaitForSeconds(m_delay);
+		m_condition = true;
+	}
+}
---
+++ Assets/Generic Logic/GameManager/GameEndScreenStateLogic.cs
@@ -0,0 +1,55 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilities.States;
+
+public class GameEndScreenStateLogic : StateLogic
+{
+	[SerializeField] private Image m_background = null;
+	[SerializeField] private TMP_Text m_text = null;
+	[Space]
+	[SerializeField] private float m_colorDuration = 1f;
+	[SerializeField] private float m_scaleInDuration = 1f;
+	[SerializeField] private float m_scaleOutDuration = .5f;
+
+	private Sequence m_backgroundSequence = null;
+	private Sequence m_textSequence = null;
+
+	private void Awake()
+	{
+		var targetColor = m_background.color;
+		var formColor = m_background.color;
+		formColor.a = 0;
+
+		m_backgroundSequence = DOTween
+			.Sequence()
+			.Append(m_background.DOColor(targetColor, m_colorDuration).From(formColor, true).SetEase(Ease.InQuad))
+			.SetAutoKill(false)
+			.Pause();
+
+		var textTransform = m_text.transform;
+		m_textSequence = DOTween
+			.Sequence()
+			.Append(textTransform.DOScale(1.2f, m_scaleInDuration).From(0, true).SetEase(Ease.InQuad))
+			.Append(textTransform.DOScale(1f, m_scaleOutDuration).SetEase(Ease.OutQuad))
+			.SetAutoKill(false)
+			.Pause();
+	}
+
+	public override void Activate()
+	{
+		base.Activate();
+		PlayTweens();
+	}
+
+	[ContextMenu("PlayTweens")]
+	public void PlayTweens()
+	{
+		m_backgroundSequence.Rewind();
+		m_backgroundSequence.Play();
+
+		m_textSequence.Rewind();
+		m_textSequence.Play();
+	}
+}
