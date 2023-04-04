0ce3c7dc848f42b862f781967699ef6daf350a6c
Łukasz <ukidabek@gmail.com>
2023-04-04 20:48:08 +0200

    [Code] Trigger enter victory condition added.

---
+++ Assets/Dedicated Logic/RemoteSwitchStateCondition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Utilities.ReferenceHost;
+using Utilities.States;
+
+public abstract class RemoteSwitchStateCondition : MonoBehaviour, ISwitchStateCondition, IInitializable
+{
+	[Inject, SerializeField] protected SwitchStateStateLogic m_switchStateLogic = null;
+
+	public abstract bool Condition { get; }
+
+	public virtual void Initialize() => m_switchStateLogic.AddCondition(this);
+
+	private void OnDestroy() => m_switchStateLogic.RemoveCondition(this);
+}
