461dd7142e3cc91e6b75bcb32fcbc1dccbffa85f
Łukasz <ukidabek@gmail.com>
2023-04-04 20:56:21 +0200

    [Code] Reference handling logic moved.

---
+++ Assets/Dedicated Logic/References/SwitchStateStateLogicReferenceInjector.cs
@@ -0,0 +1,6 @@
+﻿using Utilities.ReferenceHost;
+using Utilities.States;
+
+public class SwitchStateStateLogicReferenceInjector : ReferenceHostInjector<SwitchStateStateLogicReferenceHost, SwitchStateStateLogic>
+{
+}
