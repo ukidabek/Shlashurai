51d884592fcdd8b31e199073dc632f66e09d6fd7
Łukasz <ukidabek@gmail.com>
2023-04-19 21:13:24 +0200

    [Code] Dependency injecting Pre process added.

--- Assets/Dedicated Logic/States/StatePreProcessor.cs
+++ Assets/Dedicated Logic/States/StatePreProcessor.cs
@@ -7,7 +7,7 @@ namespace Shlashurai.Character
 	public class StatePreProcessor : MonoBehaviour, IStatePreProcessor
 	{
 		[SerializeField] private InjectionManager m_injectionManager = null;
-		public void PreProcessor(IState state)
+		public void Process(IState state)
 		{
 			if (!(state is Component component)) return;
 			var injectionPointCollection = component.GetComponent<InjectionPointCollection>();
