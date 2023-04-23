753434083d73024e34b7ceb9cfba4c45cdca99e4
Lukasz Dabek <ukidabek@gmail.com>
2023-04-23 20:08:16 +0200

    [Code] Control of default equipment setting moved to dedicated class.

---
+++ Assets/Dedicated Logic/States/ReferenceHostUsingStateLogic.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Utilities.ReferenceHost;
+using Utilities.States;
+
+namespace Shlashurai.States
+{
+	public abstract class ReferenceHostUsingStateLogic<ReferenceHostType, ReferenceType> : StateLogic where ReferenceHostType : ReferenceHost<ReferenceType> where ReferenceType : Object
+	{
+		[SerializeField] protected ReferenceHostType m_referenceHost = null;
+	}
+}
---
+++ Assets/Dedicated Logic/States/SetDefaultEquipmentSetter.cs
@@ -0,0 +1,17 @@
+﻿using Shlashurai.Items;
+using Shlashurai.References;
+
+namespace Shlashurai.States
+{
+	public class SetDefaultEquipmentSetter : ReferenceHostUsingStateLogic<DefaultEquipmentSetterReferenceHost, DefaultEquipmentSetter>
+	{
+		public override void Activate()
+		{
+			base.Activate();
+
+			if (m_referenceHost == null) return;
+			var equipmentSetter = m_referenceHost.Instance;
+			equipmentSetter.Set();
+		}
+	}
+}
